d939118e7e8223ec76d8277365eea1f1a8d9c9ec
oumaima-echriyah <oumaima.echriyah@gmail.com>
2025-01-06 18:13:49 +0100

    RAG modified

--- BackEnd/RAG2/initializeData.py
+++ BackEnd/RAG2/initializeData.py
@@ -33,7 +33,7 @@ def get_text_chunks(text):
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    vector_store.save_local("./BackEnd/RAG2/faiss_index")
 
 # Fonction principale pour initialiser les données
 def initialize_data():
